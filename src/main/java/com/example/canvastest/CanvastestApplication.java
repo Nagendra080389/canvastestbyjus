85e8a553d0183a0f9dc61804bafbcd958d41039e
Nagendra <nagendra080389@gmail.com>
2021-10-20 13:31:32 +0530

    Add commit

---
+++ src/main/java/com/example/canvastest/CanvastestApplication.java
@@ -0,0 +1,13 @@
+package com.example.canvastest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvastestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanvastestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canvastest/CanvastestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canvastest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanvastestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
